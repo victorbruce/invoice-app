52dbff9402e5e43d0d8df4ef380eec9498672148
Victor Bruce <victorbruce82@gmail.com>
2025-06-03 17:29:33 +0000

    feat: add the functionality to toggle between light and dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'invoice-app';
+  isDark = false;
+
+  constructor(private renderer: Renderer2) {}
+
+  toggleTheme() {
+    this.isDark = !this.isDark;
+
+    const themeClass = 'dark-theme';
+
+    if (this.isDark) {
+      this.renderer.addClass(document.documentElement, themeClass);
+    } else {
+      this.renderer.removeClass(document.documentElement, themeClass);
+    }
+  }
 }
