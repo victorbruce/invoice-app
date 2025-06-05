54e1c4c1ffc13cfd5da51cc0e11a98ec5898d7db
Victor Bruce <victorbruce82@gmail.com>
2025-06-05 10:39:17 +0000

    refactor: add alt attribute to app logo image

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component, Renderer2 } from '@angular/core';
 import { ThemeService } from '../../services/theme.service';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
