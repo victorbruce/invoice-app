b2b4f54194afdb510bc7966b63d19f578a65cd23
Victor Bruce <victorbruce82@gmail.com>
2025-06-08 20:50:49 +0000

    feat: invoice deletion functionality

--- src/app/pages/invoice-details/invoice-details.component.ts
+++ src/app/pages/invoice-details/invoice-details.component.ts
@@ -1,8 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 import { StatusBadgeComponent } from '../../components/status-badge/status-badge.component';
+import { ConfirmModalComponent } from '../../components/confirm-modal/confirm-modal.component';
 import { InvoiceService } from '../../services/invoice.service';
 import { Invoice } from '../../core/models/invoice.model';
 import { FormatDatePipe } from '../../shared/pipes';
@@ -10,20 +12,37 @@ import { DeviceService } from '../../services/device.service';
 
 @Component({
   selector: 'app-invoice-details',
-  imports: [CommonModule, StatusBadgeComponent, FormatDatePipe],
+  imports: [
+    CommonModule,
+    StatusBadgeComponent,
+    FormatDatePipe,
+    ConfirmModalComponent,
+  ],
   templateUrl: './invoice-details.component.html',
   styleUrl: './invoice-details.component.scss',
 })
 export class InvoiceDetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
+  router: Router = inject(Router);
   invoiceService: InvoiceService = inject(InvoiceService);
   invoice: Invoice | undefined;
   isMobile$ = inject(DeviceService).isMobile$;
+  invoiceId: string;
+  showConfirmModal = false;
 
   constructor() {
-    const invoiceId = this.route.snapshot.params['id'];
-    this.invoiceService.getInvoiceById(invoiceId).subscribe((data) => {
+    this.invoiceId = this.route.snapshot.params['id'];
+    this.invoiceService.getInvoiceById(this.invoiceId).subscribe((data) => {
       this.invoice = data;
     });
+  }
+
+  onDelete(): void {
+    this.invoiceService.deleteInvoice(this.invoiceId);
+    this.showConfirmModal = false;
+
+    setTimeout(() => {
+      this.router.navigate(['/']);
+    }, 100);
   }
 }
