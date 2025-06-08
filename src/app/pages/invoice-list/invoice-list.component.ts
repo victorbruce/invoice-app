0de1952e6c56037bd8af5531de423171992d9ebf
Victor Bruce <victorbruce82@gmail.com>
2025-06-08 08:13:15 +0000

    feat: add filter functionality

--- src/app/pages/invoice-list/invoice-list.component.ts
+++ src/app/pages/invoice-list/invoice-list.component.ts
@@ -1,27 +1,51 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule, CurrencyPipe } from '@angular/common';
+import { Component, inject, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { InvoiceService } from '../../services/invoice.service';
 import { Invoice } from '../../core/models/invoice.model';
-// import { mapInvoices } from '../../core/utils';
-import { FormatDatePipe, CapitalizePipe } from '../../shared/pipes';
+import { DropDownComponent } from '../../components/drop-down/drop-down.component';
+import { InvoiceCardComponent } from '../../components/invoice-card/invoice-card.component';
 
 @Component({
   selector: 'app-invoice-list',
-  imports: [CommonModule, FormatDatePipe, CurrencyPipe, CapitalizePipe],
+  imports: [CommonModule, DropDownComponent, InvoiceCardComponent],
   templateUrl: './invoice-list.component.html',
   styleUrl: './invoice-list.component.scss',
 })
 export class InvoiceListComponent {
   invoices: Invoice[] = [];
-  totalInvoices: number = 0;
   invoiceService: InvoiceService = inject(InvoiceService);
+  selectedStatuses: string[] = [];
+  isMobile = false;
 
   constructor() {}
 
   ngOnInit(): void {
     this.invoiceService.getInvoices().subscribe((data) => {
       this.invoices = data;
-      this.totalInvoices = this.invoices.length;
     });
+    this.updateDeviceType();
+  }
+
+  get filteredInvoices(): Invoice[] {
+    if (this.selectedStatuses.length === 0) {
+      return this.invoices;
+    }
+
+    return this.invoices.filter((invoice) =>
+      this.selectedStatuses.includes(invoice.status)
+    );
+  }
+
+  onStatusChange(statuses: string[]) {
+    this.selectedStatuses = statuses;
+  }
+
+  @HostListener('window:resize', [])
+  onResize(): void {
+    this.updateDeviceType();
+  }
+
+  private updateDeviceType(): void {
+    this.isMobile = window.innerWidth <= 768;
   }
 }
